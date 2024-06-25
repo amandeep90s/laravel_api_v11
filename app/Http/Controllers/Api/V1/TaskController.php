2643363e23fdbfa5a2221d6c7022bc0f9578fd7c
Amandeep Singh <programmeramandeep@gmail.com>
2024-06-25 19:01:51 +0530

    Validating and inserting data

--- app/Http/Controllers/Api/V1/TaskController.php
+++ app/Http/Controllers/Api/V1/TaskController.php
@@ -19,19 +19,13 @@ class TaskController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(StoreTaskRequest $request)
     {
-        //
+        $task = Task::create($request->validated());
+
+        return TaskResource::make($task);
     }
 
     /**
@@ -40,14 +34,6 @@ class TaskController extends Controller
     public function show(Task $task)
     {
         return TaskResource::make($task);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Task $task)
-    {
-        //
     }
 
     /**
